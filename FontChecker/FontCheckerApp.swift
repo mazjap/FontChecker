579eb6f649331233d763e0d60ed7ca46be58d8ba
Jordan Christensen <jordan.c4922@gmail.com>
2023-03-28 15:49:52 -0600

    Initial Commit

---
+++ FontChecker/FontCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontCheckerApp.swift
+//  FontChecker
+//
+//  Created by Jordan Christensen on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct FontCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
