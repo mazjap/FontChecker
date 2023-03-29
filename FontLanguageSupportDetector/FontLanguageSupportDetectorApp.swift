9c0ecc7422505d86d60323f791519f6ac0a9c3a1
Jordan Christensen <jordan.c4922@gmail.com>
2023-03-28 20:26:05 -0600

    Rename project

--- FontLanguageSupportDetector/FontLanguageSupportDetectorApp.swift
+++ FontLanguageSupportDetector/FontLanguageSupportDetectorApp.swift
@@ -1,11 +1,10 @@
 import SwiftUI
 
 @main
-struct FontCheckerApp: App {
+struct FontLanguageSupportDetectorApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .windowResizability(.contentMinSize)
     }
 }
